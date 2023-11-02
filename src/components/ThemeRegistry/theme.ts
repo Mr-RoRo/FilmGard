4da4512260354d1ddc081bd8af49e61efdb15460
Danial <fsoocity@gmail.com>
2023-11-02 20:01:05 +0330

    fix(theme): fix shadow values

--- src/components/ThemeRegistry/theme.ts
+++ src/components/ThemeRegistry/theme.ts
@@ -175,7 +175,7 @@ const theme = createTheme({
       index === 1
         ? "0px 1px 4px 0px rgba(0, 0, 0, 0.25)"
         : index === 2
-        ? "0px 4px 4px 0px rgba(0, 0, 0, 0.25)"
+        ? "0px 2px 4px 0px rgba(0, 0, 0, 0.25)"
         : ""
     ) as Shadows,
 });
