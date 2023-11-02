32dd5281837feda780fe17064a8c4fd4d70384b2
Danial <fsoocity@gmail.com>
2023-11-02 22:05:30 +0330

    feat(layout): add login route

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
+import Login from "../pages/Login/Login";
 const LayoutRouter = () => {
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </>
