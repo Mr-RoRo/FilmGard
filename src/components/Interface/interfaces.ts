78671acc995f040c11d20a76750dc375cf57849b
Danial <fsoocity@gmail.com>
2023-11-02 21:48:55 +0330

    feat(): add Imdb high rating Movie

---
+++ src/components/Interface/interfaces.ts
@@ -0,0 +1,18 @@
+export interface MovieRes {
+  data: {
+    country: string;
+    genres: Array<string>;
+    id: number;
+    images: Array<string>;
+    imdb_rating: string;
+    poster: string;
+    title: string;
+    year: string;
+  }[];
+  metadata: {
+    current_page: string;
+    page_count: number;
+    per_page: number;
+    total_count: number;
+  };
+}
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,16 +2,35 @@ import { Divider, Stack } from "@mui/material";
 import NavBar from "../../components/NavBar/NavBar";
 import Hero from "../../components/Hero/Hero";
 import Footer from "../../components/Footer/Footer";
+import MovieCard from "../../components/MovieCard/MovieCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { MovieRes } from "../../components/Interface/interfaces";
 
 const HomePage = () => {
+  const [responseMovie, setResponseMovie] = useState<MovieRes>();
+  useEffect(() => {
+    const fetch = () => {
+      axios
+        .get<MovieRes>("https://moviesapi.ir/api/v1/movies?page=1")
+        .then((res) => {
+          setResponseMovie(res.data);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    };
+    fetch();
+  }, []);
   return (
     <>
       <NavBar />
       <Stack mx="150px" mt="90px" mb="40px">
         <Hero />
-        <Divider sx={{ fontSize: "20px", mt: "40px" }}>
+        <Divider sx={{ fontSize: "20px", mt: "40px", mb: "25px" }}>
           فیلم های برتر IMDB
         </Divider>
+        <MovieCard data={responseMovie} />
       </Stack>
       <Footer />
     </>
