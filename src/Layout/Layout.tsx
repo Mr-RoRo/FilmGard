04c59d596a8e2d1bbcb0471bc67b289f851db3aa
Danial <fsoocity@gmail.com>
2023-11-06 20:55:54 +0330

    feat(): Create SignUp

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
 import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
 const LayoutRouter = () => {
   return (
     <>
@@ -8,6 +9,7 @@ const LayoutRouter = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Router>
     </>
