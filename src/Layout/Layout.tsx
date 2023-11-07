95509d4ab05e9dbc9768ad057882bc51c598b679
Danial <fsoocity@gmail.com>
2023-11-07 17:18:02 +0330

    feat(): del login and signup

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,15 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
-import Login from "../pages/Login/Login";
-import SignUp from "../pages/SignUp/SignUp";
+
 const LayoutRouter = () => {
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Router>
     </>
